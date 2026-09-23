79cc5a4f36c0a0824843ae074ea62748a5e9618a
Danduni123 <danieln254@nycstudents.net>
2026-09-23 10:17:11 -0400

    wasd

--- Unit2.py
+++ Unit2.py
@@ -7,9 +7,59 @@ print(values)
 for i in values:
     print(i) """
 
-rain = False
+""" rain = False
 if rain == True:
     print("Bring umbrella")
 else:
     print("No umbrella")
 
+age = 66
+if age > 65:
+    print("senior citizen")
+elif age > 18:
+    print("Adult")
+else: 
+    print("Child")
+
+has_key = False
+is_magic = True
+if has_key and is_magic:
+    print("Enter Castle")
+else:
+    print("Cannot Enter") """
+
+#integer
+#x=7
+#float
+#y=3.14
+
+#boolean
+#z = True
+
+#string
+#name = "Steve"
+
+#imput
+
+#lists
+
+#students = ["Natalie", "Martin", "Ben", "Stefania"]
+#you can also use an integer in a list
+#tudnets.append("Karas")
+#print(students[0]) 
+#for student in student:
+    #print(student)
+    #if (studen == Ben)
+        #print(student, "Found him!")
+
+""" def calculator():
+    bill = float(input("how much was the bill?"))
+    tip = int(input("How much would you like to tip? 0% / 10% / 15% / 20% / 25% (no $ sign)"))
+    tip = tip / 100
+    total = bill * (1 + tip)
+    print(f"Your total is ${total}")
+calculator() """
+
+sentence = input("Type your sentence here")
+y = sentence.split()
+print(len(y))
